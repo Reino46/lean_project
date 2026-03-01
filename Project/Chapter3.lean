951aba456dcbf45f578caedba9768dbef6e869f6
Reino46 <steliosfilip09@gmail.com>
2026-03-01 02:09:44 +0200

    started ch3.4

--- Project/Chapter3.lean
+++ Project/Chapter3.lean
@@ -215,3 +215,224 @@ end Kef33
 
 
 namespace Kef331
+
+/- προς αποδειξη του p → q → p ∧ q -/
+
+/-
+  And.intro h1 h2 χτιζει αποδειξη του p ∧ q
+  χρησιμοποιωντας αποδειξης h1 : p και h2 : q
+  ειναι το AND-INTRODUCTION RULE
+-/
+
+variable (p q : Prop)
+example (hp : p) (hq : q) : p ∧ q := And.intro hp hq
+#check fun (hp : p) (hq : q) => And.intro hp hq
+
+-- example = θεωρημα χωρις ονομα και αποθηκευση
+
+/-
+  And.left h (και And.right h)
+  δημιουργουν αποδειξη του p (αντιστοιχα του q)
+  απο αποδειξη h : p ∧ q
+  LEFT AND RIGHT-ELIMINATION RULES
+-/
+
+variable (p q : Prop)
+
+example (h : p ∧ q) : p := And.left h
+example (h : p ∧ q) : q := And.right h
+
+-- τοτε εχουμε p ∧ q → q ∧ p:
+
+example (h : p ∧ q) : q ∧ p :=
+  And.intro (And.right h) (And.left h)
+
+/-
+  ∧ ~ × μεσω CURRY - HOWARD
+  ∧ για Props (And)
+  × για Types (Prod)
+-/
+
+
+/-
+  ⟨hp, hq⟩ αντι για And.intro hp hq
+-/
+
+variable (p q :Prop)
+variable (hp : p) (hq : q)
+
+#check (⟨hp, hq⟩ : p ∧ q)
+
+
+variable (xs : List Nat)
+#check List.length xs
+#check xs.length
+-- μια συντομογραφια
+
+-- αρα η παραπανω αποδειξη γραφεται
+variable (p q : Prop)
+example (h : p ∧ q) : q ∧ p :=
+  ⟨h.right, h.left⟩
+
+-- ή ακομα μπορω να κανω flatten nested constructors ωστε ΤΑΕΙ
+variable (p q : Prop)
+example (h : p ∧ q) : q ∧ p ∧ q :=
+  ⟨h.right, ⟨h.left, h.right⟩⟩
+example (h : p ∧ q) : q ∧ p ∧ q :=
+  ⟨h.right, h.left, h.right⟩
+
+end Kef331
+
+------------------------------- 3.3.2. DISJUNCTION ------------------------------------
+
+namespace Kef332
+
+/-
+  Or.intro_left q hp κατασκευαζει αποδειξη του p ∨ q
+  απο αποδειξη hp : p
+  ομοια Or.intro_right p hq
+  LEFT AND RIGHT OR-INTRODUCTION RULES
+-/
+
+variable (p q : Prop)
+example (hp : p) : p ∨ q := Or.intro_left q hp
+example (hq : q) : p ∨ q := Or.intro_right p hq
+
+/-
+  Για το OR-ELIMINATION RULE:
+  μπορω να αποδειξω το r απο το p ∨ q
+  αποδεικνυοντας οτι p → r και οτι q → r
+-/
+
+/-
+  Στην εκφραση Or.elim hpq hpr hqr
+  το Or.elim δεχεται 3 συνιστωσες hpq : p ∨ q, hpr : p → r, hqr : q → r
+  και δινει μια αποδειξη του r
+-/
+
+-- θδο p ∨ q → q ∨ r
+
+variable (q p r : Prop)
+
+#check Or.elim
+
+example (h : p ∨ q) : q ∨ p :=
+  Or.elim h
+    (fun hp : p =>
+      show q ∨ p from Or.intro_right q hp)
+    (fun hq : q =>
+      show q ∨ p from Or.intro_left p hq)
+/-
+  Απόδειξη p ∨ q → q ∨ p μέσω Ανάλυσης Περιπτώσεων (Or.elim).
+  Εξετάζουμε ξεχωριστά την περίπτωση του p και την περίπτωση του q,
+  χρησιμοποιώντας τους κανόνες εισαγωγής (intro_left/right) για να χτίσουμε τον στόχο.
+-/
+
+-- και με συντομογραφιες:
+example (h : p ∨ q) : q ∨ p :=
+  h.elim (fun hp => Or.inr hp) (fun hq => Or.inl hq)
+
+end Kef332
+
+
+-------------------------- 3.3.3 NEGATION AND FALSITY --------------------------------
+
+namespace Kef333
+
+-- ¬p οριζεται ως p → False
+
+-- η εκφραση hnp hp παραγει μια αποδειξη του False απο hp : p και hnp : ¬p
+
+-- θδο (p → q) → ¬q → ¬p
+variable (p q : Prop)
+example (hpq : p → q) (hnq : ¬q) : ¬p :=
+  fun hp : p =>
+  show False from hnq (hpq hp)
+
+/-
+  False.elim εκφραζει οτι τα παντα αποδεικνυονται αν εχω contradiction
+-/
+
+example (hp : p) (hnp : ¬p) : q := False.elim (hnp hp)
+-- το q οτιδηποτε:
+example (hp : p) (hnp : ¬p) : q := absurd hp hnp
+
+-- θδο ¬p → q → (q → p) → r
+variable (p q r : Prop)
+example (hnp : ¬p) (hq : q) (hqp : q → p) : r :=
+  absurd (hqp hq) hnp
+
+/-
+  το False εχει ΜΟΝΟ ELIMINATION RULE
+  το True εχει ΜΟΝΟ INTRODUCTION RULE, True.intro
+  δηλ. το True ειναι απλα αληθεια και εχει κανονικη αποδειξη, την True.intro
+-/
+
+end Kef333
+
+
+----------------------------- 3.3.4. LOGICAL EQUIVALENCE -------------------------------
+
+namespace Kef334
+
+/-
+  Η εκφραση Iff.intro h1 h2
+  παραγει μια αποδειξη του p ↔ q
+  απο h1 : p → q
+  και h2 : q → p
+-/
+
+/-
+  H εκφραση Iff.mp h
+  παραγει μια αποδειξη του p → q
+  απο το h : p ↔ q
+-/
+
+/-
+  Αντιστοιχα η Iff.mpr h
+  παραγει μια αποδειξη του q → p
+  απο το h : p ↔ q
+-/
+
+-- θδο p ∧ q ↔ q ∧ p
+variable (p q : Prop)
+
+theorem and_swap : p ∧ q ↔ q ∧ p :=
+  Iff.intro
+    (fun h : p ∧ q =>
+     show q ∧ p from And.intro (And.right h) (And.left h))
+    (fun h : q ∧ p =>
+     show p ∧ q from And.intro (And.right h) (And.left h))
+
+#check and_swap p q
+
+variable (h : p ∧ q)
+example : q ∧ p := Iff.mp (and_swap p q) h
+
+/-
+  and_swap: Απόδειξη της μεταθετικότητας του "∧" (And).
+  Iff.intro: Ο κανόνας εισαγωγής για το "↔". Απαιτεί δύο αποδείξεις (P → Q και Q → P).
+  Iff.mp (modus ponens): Χρησιμοποιεί την ισοδυναμία για να μετατρέψει μια απόδειξη
+     του (p ∧ q) σε απόδειξη του (q ∧ p).
+-/
+
+-- παλι μπορω να χρησιμοποιησω συντομογραφιες, αλλα με μπερδευουν γιατι δε μπορω να τα διαβασω καθαρα
+
+end Kef334
+
+
+-------------------------- 3.4. INTRODUCING AUXILIARY SUBGOALS -------------------------
+
+namespace Kef34
+
+/-
+  Οταν εχω μεγαλες αποδειξεις, τις σπαω σε μικροτερα κομματια
+  δηλ, με το have δημιουργω νεο ΠΡΟΣΩΡΙΝΟ ΥΠΟΣΤΟΧΟ
+-/
+
+variable (p q : Prop)
+
+example (h : p ∧ q) : q ∧ p :=
+  have hp : p := h.left
+  have hq : q := h.right
+  show q ∧ p from And.intro hq hp
